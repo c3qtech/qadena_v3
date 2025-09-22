b50b8ec2bca4df4018b2b9b3830670262f24a42e
Al Villarica <alvillarica@gmail.com>
2025-09-22 13:32:18 +0800

    fix: add noninteractive mode and -y flag for unattended apt installations

--- ubuntu/setup_qadena_build.sh
+++ ubuntu/setup_qadena_build.sh
@@ -6,6 +6,9 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
+# set noninteractive for apt
+export DEBIAN_FRONTEND=noninteractive
+
 # zsh
 apt-get install -y zsh
 
@@ -67,7 +70,7 @@ apt-get install -y apache2-utils
 
 # Add Docker's official GPG key:
 apt-get update
-apt-get install ca-certificates curl
+apt-get install -y ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
