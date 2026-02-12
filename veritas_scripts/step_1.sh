66bbc0925ab417f76a8c532b47e603d8bdd5be24
alvillarica <alvillarica@gmail.com>
2026-02-12 17:54:48 +0800

    Fix typo in treasury mnemonic variable

--- veritas_scripts/step_1.sh
+++ veritas_scripts/step_1.sh
@@ -134,7 +134,7 @@ jq -n --arg pioneer "$pioneer" --arg count "$count" --arg email "$email" --arg a
 echo "-------------------------"
 echo "Setting up $treasuryname"
 echo "-------------------------"
-$qadenaproviderscripts/setup_treasury.sh --treasury-name $treasuryname --treasury-mnemonic $treasurymnemoni
+$qadenaproviderscripts/setup_treasury.sh --treasury-name $treasuryname --treasury-mnemonic $treasurymnemonic
 c
 echo "Send this information to QFI"
 echo "$treasuryname Qadena address:  $(qadenad_alias keys show $treasuryname --address)"
